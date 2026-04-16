b2e11146b59cb9532b937754b3db00b95a4b77d2
Han <35824102+dash12653@users.noreply.github.com>
2026-04-16 18:41:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,33 +23,43 @@ def parse_data(filename):
     return points_list
 
 def main():
-    # 1. parse the csv file.
-    filename = "data/dataset.csv"
+    # 1. parse the training data file.
+    filename = "data/train_dataset.csv"
     points_list = parse_data(filename)
 
-    # 2. variables to declare.
-    target_point = (8.5, 5.5)  # the point we want to classify.
+    # 2. parse the testing data file.
+    filename = "data/test_dataset.csv"
+    target_points_list = parse_data(filename)
+    right_predictions = 0
     k = 6  # k
 
-    # 3. get top k nearest neighbors for target_node.
-    top_k_neighbors = cal_topk_neighbors(points_list, target_point, k)
+    for idx, target_point in enumerate(target_points_list):
+        # 3. extract x and y for target_point
+        target_xy = (target_point['x'], target_point['y'])
+        target_label = (target_point['label'])
 
-    # 4. print tok k neighbors.
-    print("\nTop K Neighbors: ")
-    for i, item in enumerate(top_k_neighbors):
-        point = item['point']
-        dist = item['distance']
-        print(f"Top {i + 1}: point({point['x']}, {point['y']}) | label: {point['label']} | distance: {dist:.4f}")
+        # 4. get top k nearest neighbors for target_node.
+        top_k_neighbors = cal_topk_neighbors(points_list, target_xy, k)
 
-    # 5. Store the predicted label in a variable.
-    predicted_label = vote_for_label(top_k_neighbors)
+        # 5. print tok k neighbors.
+        print("\nTop K Neighbors: ")
+        for i, item in enumerate(top_k_neighbors):
+            point = item['point']
+            dist = item['distance']
+            print(f"Top {i + 1}: point({point['x']}, {point['y']}) | label: {point['label']} | distance: {dist:.4f}")
 
-    # 6. Print the predicted label.
-    print(f"Label: {predicted_label}")  
+        # 6. Vote for label.
+        predicted_label = vote_for_label(top_k_neighbors)
 
-    # 7. Show the visualization.
-    plot_knn(points_list, target_point, top_k_neighbors, predicted_label)
+        if predicted_label == target_label:
+            right_predictions += 1
 
+        # 7. Print the predicted label.
+        print(f"Predicted_Label: {predicted_label}, Original_Label: {target_label}")
 
+        # 8. Show the visualization.
+        # plot_knn(points_list, target_xy, top_k_neighbors, predicted_label)
+
+    print(f"\nprediction correct rate: {float(right_predictions / 30)}")
 if __name__ == "__main__":
     main()
